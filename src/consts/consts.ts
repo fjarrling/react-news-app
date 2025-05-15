7d9384712e6445b1902b4d5eba3b0e3edf669cd6
fjarrling <fjarrling@gmail.com>
2025-05-15 17:25:47 +0300

    fixet infinite re-render loop, decomposed totalpages to consts, added usecallback to usefetching custom hook

--- src/consts/consts.ts
+++ src/consts/consts.ts
@@ -1,2 +1,5 @@
+import {getTotalPagesCount} from "@/utils/utils.ts";
+
 export const TOTAL_RESULTS = 99
 export const PAGE_SIZE = 9
+export const TOTAL_PAGES = getTotalPagesCount(PAGE_SIZE, TOTAL_RESULTS)
